79c032424acb96fc0d456e0b9617205f3d41672f
Andrei Bogdan Fara <afara@servoy.com>
2021-11-17 14:33:06 +0200

    SVY-16353 [NG2] port over full calendar
    removed old full calendar demo & sample (new ones will be created)

--- svyFullCalendar$model/datasources/fullcalendar/resources_calculations.js
+++
@@ -1,6 +0,0 @@
-/**
- * @properties={type:12,typeid:36,uuid:"087DC461-623A-4AE6-8450-33BFC2FE1E02"}
- */
- function colorlabel() {
-		return '<div style="background-color:' + background_color +';">&nbsp;</div>';
-	}
